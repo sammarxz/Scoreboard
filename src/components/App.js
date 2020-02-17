4d203a75ff9bd85055aac08eae29348dadfe4394
Sam Marxz <sammarxzz@gmail.com>
2020-02-17 19:02:19 -0300

    Add 'Stats' Component

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={ this.state.players.length } />
+        <Header title="Scoreboard" players={ this.state.players } />
         { this.state.players.map((player, index) => (
           <Player 
             key={ player.id } 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Stats from './Stats';
 
 
 const Header = (props) => (
   <header>
+  	<Stats players={ props.players } />
     <h1>{ props.title }</h1>
-    <div className="stats">Players: { props.totalPlayers }</div>
   </header>
 )
 
